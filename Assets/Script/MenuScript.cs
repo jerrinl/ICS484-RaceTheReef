d17accaddd43b23f2ec94d4dd4d6ef018e28b27c
Jerrin <jerrinl@hawaii.edu>
2016-04-29 00:57:16 -1000

    menus

---
+++ Assets/Script/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public Button startText;
+    
+   
+
+	// Use this for initialization
+	void Start () {
+        startText = startText.GetComponent<Button>();
+
+	}
+	
+    public void StartPress()
+    {
+        startText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        	
+	}
+}   
